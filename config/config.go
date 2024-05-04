eba8d01c9e985d5d36d1c463eb47668ffebcba93
Anh <phuonganhniie@gmail.com>
2024-05-04 09:47:16 +0700

    refactor: config file for both envs

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"os"
+
+	"github.com/phuonganhniie/botbot-leetcode/internal/logger"
 	"github.com/spf13/viper"
 )
 
@@ -10,28 +13,32 @@ type Config struct {
 	TelegramChatID   string `mapstructure:"TELEGRAM_CHAT_ID"`
 }
 
-var config *Config
-
 func LoadConfig() (*Config, error) {
+	viper.SetConfigType("env")
 	viper.AutomaticEnv()
+
+	if _, err := os.Stat("config/config.env"); os.IsNotExist(err) {
+		logger.Info("No local .env file found. Relying on environment variables.")
+	} else {
+		viper.SetConfigName("config")
+		viper.AddConfigPath("config")
+
+		if err := viper.ReadInConfig(); err != nil {
+			logger.Errorf("Error reading config file: %v\n", err)
+		} else {
+			logger.Info("Config file loaded")
+		}
+	}
 
 	viper.BindEnv("LEETCODE_DAILY_URL")
 	viper.BindEnv("TELEGRAM_BOT_TOKEN")
 	viper.BindEnv("TELEGRAM_CHAT_ID")
 
-	viper.SetConfigName("config")
-	viper.SetConfigType("env")
-	viper.AddConfigPath(".")
-
-	err := viper.ReadInConfig()
+	var config Config
+	err := viper.Unmarshal(&config)
 	if err != nil {
 		return nil, err
 	}
 
-	err = viper.Unmarshal(&config)
-	if err != nil {
-		return nil, err
-	}
-
-	return config, nil
+	return &config, nil
 }
--- internal/format/telegram_formatter.go
+++ internal/format/telegram_formatter.go
@@ -11,6 +11,6 @@ type TelegramFormatter struct{}
 
 func (f *TelegramFormatter) FormatMessage(challenge *model.Challenge) string {
 	currentDate := time.Now().Format("January 2, 2006")
-	return fmt.Sprintf("Hi, today is %s, and time to s(t)uck with this problem. But don't worry, you'll be fine, and you'll be learnt.\n\nID: %s\nProblem: %s\nDifficulty: %s\nLet's be in: %s",
+	return fmt.Sprintf("Gút mỏning, today is %s, and time to s(t)uck with this problem. But don't worry, you'll be fine, and you'll be learnt.\n\nID: %s\nProblem: %s\nDifficulty: %s\nLet's be in: %s",
 		currentDate, challenge.Id, challenge.Name, challenge.Difficulty, challenge.QuestionLink)
 }
