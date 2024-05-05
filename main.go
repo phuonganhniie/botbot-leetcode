7228c5fd7bd798bedafce86d421be7604ab8c5fe
Anh <phuonganhniie@gmail.com>
2024-05-05 23:56:31 +0700

    fix: add lambda

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/phuonganhniie/botbot-leetcode/cmd"
 )
 
 func main() {
-	// lambda.Start(cmd.Start)
-	cmd.Start()
+	lambda.Start(cmd.Start)
 }
