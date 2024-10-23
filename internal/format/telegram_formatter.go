5d21454b9c2e9b6ec1ac382bc37a92391dd4fba2
Anh Do <phuonganhniie@gmail.com>
2024-10-23 16:58:00 +0700

    update content

--- internal/format/telegram_formatter.go
+++ internal/format/telegram_formatter.go
@@ -11,6 +11,6 @@ type TelegramFormatter struct{}
 
 func (f *TelegramFormatter) FormatMessage(challenge *model.Challenge) string {
 	currentDate := time.Now().Format("January 2, 2006")
-	return fmt.Sprintf("Gút mỏning, today is %s, and time to s(t)uck with this problem. But don't worry, you'll be fine, and you'll be learnt.\n\nID: %s\nProblem: %s\nDifficulty: %s\nLet's be in: %s",
+	return fmt.Sprintf("Gút mỏning, hôm nay là %s, làm nhẹ bài LeetCode nhá. Si sợ không? Không sợ thì chiến!\n\nID: %s\nVấn đề: %s\nĐộ căng: %s\nZô: %s",
 		currentDate, challenge.Id, challenge.Name, challenge.Difficulty, challenge.QuestionLink)
 }
